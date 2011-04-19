d2e556f8065ad771ceacc236dd662b559f7bbd0a
Robert Gaal <robert@wakoopa.com>
2011-04-19 12:50:09 +0200

    Redirect fixed

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,11 @@
+class PageController < ActionController::Base
+  before_filter :redirect_all
+
+  def redirect_all
+    # HTTP 301 is a "permanent" redirect (could also contain request.env['HTTP_HOST'])
+    redirect_to "http://53miles.com", :status => 301
+  end
+
+  def index
+  end
+end
