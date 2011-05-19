e065a14817b0f79f27732883082a19d80db96b00
Robert Gaal <robert@wakoopa.com>
2011-05-19 09:36:05 +0200

    Changed to robertgaal.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_filter :redirect_all
-
-  def redirect_all
-    # HTTP 301 is a "permanent" redirect
-    redirect_to "http://53miles.com?d=" + request.env['HTTP_HOST'], :status => 301
-  end
 end
